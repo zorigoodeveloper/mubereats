8e2ea240d84aae24761c9c7bf4126332b9576912
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2026-01-12 13:03:05 +0800

    driver

--- api/driverappAPIs/urls.py
+++ api/driverappAPIs/urls.py
@@ -4,5 +4,5 @@ from .view import SignUpView, SignInView, ProfileView
 urlpatterns = [
     path('auth/signup/driver/', SignUpView.as_view(), name='signup'),
     path('auth/signin/driver/', SignInView.as_view(), name='signin'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('auth/profile/driver/', ProfileView.as_view(), name='profile'),
 ]
--- api/driverappAPIs/view.py
+++ api/driverappAPIs/view.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .serializers import SignUpSerializer, SignInSerializer
+from ..serializers import SignUpSerializer, SignInSerializer
 from ..database import execute_query, execute_insert
 from ..auth import hash_password, verify_password, create_access_token, JWTAuthentication
 
