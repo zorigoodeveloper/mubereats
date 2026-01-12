857858e2e9c27756dc5a12213db0a3d9cdfd8de1
Kami777Navi <khusledhair@gmail.com>
2026-01-12 15:52:27 +0800

    hurgelt tuluv

--- api/driverappAPIs/urls.py
+++ api/driverappAPIs/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .view import SignUpView, SignInView, ProfileView
+from .delivery import DeliveryStatusView
 
 urlpatterns = [
     path('auth/signup/driver/', SignUpView.as_view(), name='signup'),
     path('auth/signin/driver/', SignInView.as_view(), name='signin'),
     path('auth/profile/driver/', ProfileView.as_view(), name='profile'),
+    path('deliveries/', DeliveryStatusView.as_view(), name='delivery-status'),
+
 ]
