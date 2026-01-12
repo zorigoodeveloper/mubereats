412214154db4330340bf9435d441c707ba7973f4
svhe4u <sukheebt@gmail.com>
2026-01-12 15:42:14 +0800

    Tengis order

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('auth/signin/', SignInView.as_view(), name='signin'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('', include('api.driverappAPIs.urls')),
+    path('', include('api.userappAPIs.urls')),
+
 ]
