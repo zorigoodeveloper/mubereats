e6649f20f2fb4261e0b8e9b99629d566b40325fa
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2026-01-12 13:31:34 +0800

    driver

--- api/driverappAPIs/serializers.py
+++ api/driverappAPIs/serializers.py
@@ -6,8 +6,6 @@ class SignUpSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True, min_length=6)
     full_name = serializers.CharField(max_length=255)
     user_type = serializers.ChoiceField(choices=['driver'])
-
-    # Optional fields for driver
     license_number = serializers.CharField(max_length=50, required=False)
     vehicle_type = serializers.CharField(max_length=50, required=False)
     vehicle_plate = serializers.CharField(max_length=20, required=False)
