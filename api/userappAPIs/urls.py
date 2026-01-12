3ec459bd8f2d6c1330dfc3ac74f85b41491017a6
Sumka666 <sw23d015@mandakh.edu.mn>
2026-01-12 15:55:34 +0800

    tengis

--- api/userappAPIs/urls.py
+++ api/userappAPIs/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import CustomerSignUpView, SignInView, ProfileView, UserSearchAPIView, ProfileUpdateView
 
 urlpatterns = [
-    path('auth/signup/customer', CustomerSignUpView.as_view(), name='signup'),
-    path('auth/signin/customer', SignInView.as_view(), name='signin'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('auth/signup/customer/', CustomerSignUpView.as_view(), name='signup'),
+    path('auth/signin/customer/', SignInView.as_view(), name='signin'),
     path("user/search/", UserSearchAPIView.as_view(), name="user-search"),
     path('auth/profile/customer/', ProfileView.as_view(), name='profile'),
     path('auth/profile/update/customer/', ProfileUpdateView.as_view(), name='profile'),
