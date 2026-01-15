978651f43ca17d54d9adac316ee7d5a6f3a4b507
Sumka666 <sw23d015@mandakh.edu.mn>
2026-01-15 11:44:18 +0800

    a

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
+    
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
     'django.contrib.auth', 
